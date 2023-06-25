b531a46c86ed07875bbc36dcfff22d01741ec0b3
hotelinphone <victor.bernardez@kilotec.com>
2023-06-26 00:25:06 +0200

    Primera subida de documentos para cursos de Javascript para los chcios

---
+++ jscript.js
@@ -0,0 +1,19 @@
+function sumar(a, b) {
+  return a + b;
+}
+
+let y = 0;
+for (let x = 1; x <= 100; x++) {
+  y = y + x;
+}
+
+alert(y);
+
+y = 0;
+x = 1;
+while (x <= 100) {
+  y = y + x;
+  x++;
+}
+
+alert(y);
